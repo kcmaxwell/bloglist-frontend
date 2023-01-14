d48baeda0aada15b427259c1b4704a0991f2db79
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-01-13 23:59:37 -0700

    Finish exercise 5.17

---
+++ cypress/e2e/blog_app.cy.js
@@ -0,0 +1,10 @@
+describe('Blog app', () => {
+  beforeEach(() => {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', () => {
+    cy.contains('Login');
+  });
+});
